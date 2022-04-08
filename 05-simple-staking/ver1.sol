35cf63ad2d81268565bf4099a49e24d26f0d7d60
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-04-08 20:21:33 +0430

    Update ver1.sol

--- 05-simple-staking/ver1.sol
+++ 05-simple-staking/ver1.sol
@@ -19,12 +19,3 @@ contract SimpleStaking {
   }
 }
 
-
-// deploy this token for test
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract TestToken is ERC20 {
-    constructor(string memory _name, string memory _symbol) 
-    ERC20(_name, _symbol) {
-        _mint(msg.sender, 1000000000000000000000);
-    }
-}
