0f3b009b37ed27958cebe94c2a1eadf6a537098f
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-05-04 16:35:02 +0430

    Update AbstractERC165.sol

--- 07-interfaceChceker/AbstractERC165.sol
+++ 07-interfaceChceker/AbstractERC165.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8;
 
+import './IERC165.sol';
+
 abstract contract AERC165 is IERC165 {
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
         return interfaceId == type(IERC165).interfaceId;
