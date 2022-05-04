19c332a521ee1b3692e191c75c8cdfa436b6c6e7
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-05-04 16:34:41 +0430

    Update ERC165.sol

--- 07-interfaceChceker/ERC165.sol
+++ 07-interfaceChceker/ERC165.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8;
 
+import './IERC165.sol';
+
 contract ERC165 is IERC165 {
     mapping(bytes4 => bool) private _supportedInterfaces;
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
