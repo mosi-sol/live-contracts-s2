f1fc29e66ae3e2bebf0ecbedfa0f671748fe2dc8
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-05-27 09:47:18 +0430

    Create ver4.sol

---
+++ 08-vault-erc20/ver4.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8;
+
+contract Vault {
+    IERC20 public token;
+    // requirment: add a mapping for each erc20 token. (i not add that here)
+    
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor() {
+        
+    }
+
+    function _mint(address _to, uint _shares) private {
+        totalSupply += _shares;
+        balanceOf[_to] += _shares;
+    }
+
+    function _burn(address _from, uint _shares) private {
+        totalSupply -= _shares;
+        balanceOf[_from] -= _shares;
+    }
+
+    function deposit(uint _amount, address _token) external returns (bool) {
+        uint shares;
+        token = IERC20(_token);
+        if (totalSupply == 0) {
+            shares = _amount;
+            return false;
+        } else {
+            shares = (_amount * totalSupply) / token.balanceOf(address(this));
+        }
+
+        _mint(msg.sender, shares);
+        token.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function withdraw(uint _shares, address _token) external returns (bool) {
+        token = IERC20(_token);
+        uint amount = (_shares * token.balanceOf(address(this))) / totalSupply;
+        if (amount < 0) return false;
+        _burn(msg.sender, _shares);
+        token.transfer(msg.sender, amount);
+    }
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint amount);
+    event Approval(address indexed owner, address indexed spender, uint amount);
+}
