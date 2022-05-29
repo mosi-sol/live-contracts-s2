83e96ce88a44d3380915708ffc0a35974d83c2b0
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-05-29 20:40:19 +0430

    Update ver1.sol

--- 09-batch/ver1.sol
+++ 09-batch/ver1.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Standard is ERC20 {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        // mint some token
+    }
     
+    // check amount < minted
+    // check address hasExist
      function batchTransfer(address[] calldata _toList, uint256[] calldata _amounts) external returns (uint) {
         for (uint256 i = 0; i < _toList.length; i++) {
             transfer(_toList[i], _amounts[i]);
