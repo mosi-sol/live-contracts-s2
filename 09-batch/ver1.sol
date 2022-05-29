1987240f53ea077053146bc8e83c1f702758f49b
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-05-29 20:17:58 +0430

    Create ver1.sol

---
+++ 09-batch/ver1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Standard is ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+    
+     function batchTransfer(address[] calldata _toList, uint256[] calldata _amounts) external returns (uint) {
+        for (uint256 i = 0; i < _toList.length; i++) {
+            transfer(_toList[i], _amounts[i]);
+        }
+        return _toList.length;
+    }
+
+}
