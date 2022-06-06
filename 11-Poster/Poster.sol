dfe102cebfb4891f27e65387d2a2a9768249d96d
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-06-06 12:40:57 +0430

    Update Poster.sol

--- 11-Poster/Poster.sol
+++ 11-Poster/Poster.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8;
 
 /// @dev https://eips.ethereum.org/EIPS/eip-3722
+
+// interface
 interface IPoster {
 
     event NewPost(address indexed user, string content, string indexed tag);
@@ -9,11 +11,20 @@ interface IPoster {
     function post(string calldata content, string calldata tag) external;
 }
 
+// usual
 contract Poster {
 
     event NewPost(address indexed user, string content, string indexed tag);
 
     function post(string calldata content, string calldata tag) public {
+        emit NewPost(msg.sender, content, tag);
+    }
+}
+
+// follow the terms
+contract Poster is IPoster {
+
+    function post(string calldata content, string calldata tag) public override {
         emit NewPost(msg.sender, content, tag);
     }
 }
